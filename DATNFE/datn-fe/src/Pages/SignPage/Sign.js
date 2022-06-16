e7b975ce8e35cb03c655670662e6aef80db26114
phamtien98 <minhtienpham1998@gmail.com>
2022-06-16 16:12:03 +0700

    del all

--- DATNFE/datn-fe/src/Pages/SignPage/Sign.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Sign = () => {
-  return (
-    <div>
-      <p>Sign pagee</p>
-    </div>
-  );
-};
-export default Sign;
--- DATNFE/datn-fe/src/Pages/ValidateSignaturePage/ValidateSignaturePage.js
+++
@@ -1,9 +0,0 @@
-import react from "react";
-const ValidateSignaturePage = () =>{
-    return (
-        <div>
-          <p>Validate PAge</p>  
-        </div>
-    )
-}
-export default ValidateSignaturePage;
